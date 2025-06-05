b63d939f2f1861991412e5cea3a3ff53055d9cd6
venkatasrinivas8688 <pamidisrinivas4@gmail.com>
2025-06-05 13:55:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import LandingPage from "./vendorDashboard/pages/LandingPage";
 import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import NavBar from "./vendorDashboard/components/NavBar";
-import Login from "./vendorDashboard/components/forms/Login";
+import LandingPage from "./vendorDashboard/pages/LandingPage";
 import NotFound from "./vendorDashboard/components/NotFound";
 
 const App = () => {
