49f7cc485d5126148f97d092b9c8d4aaf877e635
venkatasrinivas <pamidisrinivas4@gmail.com>
2025-06-05 13:26:23 +0530

    frontend code added

---
+++ src/vendorDashboard/components/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Welcome = () => {
+  const firmName = localStorage.getItem("firmName");
+
+  return (
+    <div className="welcomeSection">
+      <h2>Welcome {firmName}</h2>
+      <div className="landingImage">
+        <img src="/assets/chef.jpg" alt="welcome" />
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
